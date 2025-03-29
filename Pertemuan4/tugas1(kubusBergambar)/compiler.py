85a00a1642476481139afa2228c46de40cfb4452
Doctor3131 <sirielwnf@gmail.com>
2025-03-29 09:00:17 +0700

    push semua file

---
+++ Pertemuan4/tugas1(kubusBergambar)/compiler.py
@@ -0,0 +1,59 @@
+# import subprocess
+
+# # use glut and glew
+# file_name = "app"
+# include_dir = "dependencies/include"
+# lib_dir = "dependencies/lib"
+# lib = "-lfreeglut -lglfw3 -lglew32s -lopengl32 -lgdi32"
+# preprocessor = "-DGLEW_STATIC"
+
+# def main():
+#     # Compile source files
+#     subprocess.call(f"g++ -c main.cpp -I {include_dir} {preprocessor} -o main.o", shell=True)
+
+#     # Link object files
+#     subprocess.call(f"g++ main.o -o {file_name} -L {lib_dir} {lib} -Wl,--allow-multiple-definition", shell=True)
+
+# if __name__ == "__main__":
+#     main()
+
+# use glad
+# file_name = "app"
+# include_dir = "dependencies/include"
+# lib_dir = "dependencies/lib"
+# lib = "-lglfw3 -lopengl32 -lgdi32"  
+
+# def main():
+
+#     subprocess.call(f"g++ -c main.cpp -I {include_dir} -o main.o", shell=True)
+#     subprocess.call(f"g++ -c glad.c -I {include_dir} -o glad.o", shell=True)
+    
+#     subprocess.call(f"g++ main.o glad.o -o {file_name} -L {lib_dir} {lib}", shell=True)
+
+# if __name__ == "__main__":
+#     main()
+
+import subprocess
+import shutil
+
+# use glut and glew
+file_name = "app"
+include_dir = "dependencies/include"
+lib_dir = "dependencies/lib"
+lib = "-lfreeglut -lglfw3 -lglew32s -lopengl32 -lgdi32 -lglu32"
+preprocessor = "-DGLEW_STATIC"
+
+def main():
+    # Compile source files
+    subprocess.call(f"g++ -c main.cpp -I {include_dir} {preprocessor} -o main.o", shell=True)
+    subprocess.call(f"g++ -c imageloader.cpp -I {include_dir} {preprocessor} -o imageloader.o", shell=True)
+    subprocess.call(f"g++ -c glad.c -I {include_dir} {preprocessor} -o glad.o", shell=True)
+
+    # Link object files
+    subprocess.call(f"g++ main.o imageloader.o glad.o -o {file_name}.exe -L {lib_dir} {lib} -Wl,--allow-multiple-definition", shell=True)
+
+    # shutil.copy("sample_640x426.bmp", ".")
+
+
+if __name__ == "__main__":
+    main()
